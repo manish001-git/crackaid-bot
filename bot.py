63f524b84b8a1f814492f52c9c528a19a527bf8f
CrackAid Admin <studymentor02100@gmail.com>
2025-04-05 14:42:32 +0530

    Initial commit

---
+++ bot.py
@@ -0,0 +1,27 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackContext
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+def start(update: Update, context: CallbackContext):
+    keyboard = [
+        [InlineKeyboardButton("SSC", callback_data='ssc')],
+        [InlineKeyboardButton("Railway", callback_data='railway')],
+        [InlineKeyboardButton("Banking", callback_data='banking')],
+        [InlineKeyboardButton("Current Affairs", callback_data='current')],
+        [InlineKeyboardButton("Contact", url='https://t.me/your_contact')],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text("Welcome to CrackAid Quiz Bot! 👇 Select a category:", reply_markup=reply_markup)
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
